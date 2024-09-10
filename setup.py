1190b941327139b7b7eda1202a69d52d10b782b5
DanPark13 <dpark0703@gmail.com>
2024-09-10 16:57:19 -0400

    blank project

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'warmup_project'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='deep13',
+    maintainer_email='dpark0703@gmail.com',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'hello_world = warmup_project.hello_world:main'
+        ],
+    },
+)
